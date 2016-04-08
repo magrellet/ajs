03730a4550380e56faa7cf1284883ba044f596cf
magrellet <martin.alejandro.grellet@gmail.com>
2016-04-07 23:26:16 -0300

    ng model. Binding, controllers and scopes

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,6 +1,18 @@
-var app = angular.module("MyFirstApp",[]); //que aplicacion estamos usando
+var app = angular.module("MyFirstApp",[]); //que aplicacion estamos usando y dentro de [] los modulos que necesitamos
 
 app.controller("FirstController", function($scope){
 	$scope.nombre = "Martin";
+	$scope.comentarios = [{
+		comentario: "Buen dia",
+		usuario: "usuario1"
+	},{
+		comentario: "buenas noches",
+		usuario:"usuario2"
+	}];
+
+	$scope.agregarComentario = function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+
+	}
 
 });
