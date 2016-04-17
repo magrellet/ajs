323088302e55ec08cb49ca48b7c0a463d33e7675
magrellet <martin.alejandro.grellet@gmail.com>
2016-04-17 20:12:45 -0300

    adding post method to add a post in the list of info from json

--- http/controller.js
+++ http/controller.js
@@ -1,12 +1,27 @@
 angular.module("MyFirstApp",[])
 	.controller("FirstController", function ($scope, $http){
 		$scope.posts=[];
+		$scope.addPost = {};
 		$http.get("http://jsonplaceholder.typicode.com/posts")
 			.success(function(data){
 				$scope.posts=data;				
 			})
-			.error(function(data){
+			.error(function(error){
 				alert("error");
 			});
-
+		$scope.addPost = function(){
+			$http.post("http://jsonplaceholder.typicode.com/posts", {
+				title: $scope.newPost.title,
+				body:  $scope.newPost.body,
+				userId: 1
+			}) //en post ponemos los parametros
+			.success(function(data, status, header, config){
+				$scope.posts.push($scope.newPost);
+				console.log($scope.posts);
+				//$scope.posts = {};
+			})
+			.error(function(data, status, header, config){
+				alert("error");
+			});
+		}
 	});
